7674c9dd49a92d9e0842a7b910ca3e27d821047c
cho.chief <k.n.svetlakov@yandex.ru>
2018-05-16 13:21:00 +0400

    flag

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	addr = flag.String("addr", "localhost", "Domain address")
+	port = flag.Int("port", 6969, "Port")
+	ssl  = flag.Bool("ssl", false, "Use SSL")
+)
+
+// go run main.go --ssl=true --addr="whatsyourna.me" --port=8888
 
 func main() {
+	flag.Parse()
 	fmt.Println("wyname", "ok")
+	fmt.Println("addr", *addr)
+	fmt.Println("port", *port)
+	fmt.Println("ssl", *ssl)
 }
