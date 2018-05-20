238b8f306b4614ea0d813617e7517b9a7cb28b52
cho.chief <k.n.svetlakov@yandex.ru>
2018-05-20 16:11:46 +0400

    117 flag dep

--- main.go
+++ main.go
@@ -3,48 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
-	"wyname/kernel"
-	"wyname/utils"
 )
 
-var (
-	addr   = flag.String("addr", "localhost", "Domain address")
-	port   = flag.Int("port", 6969, "Port")
-	ssl    = flag.Bool("ssl", false, "Use SSL")
-	origin string
-)
+var addr = flag.String("addr", ":6969", "http service address")
 
-// go run main.go --ssl=true --addr="whatsyourna.me" --port=8888
+// go run main.go --addr="whatsyourna.me:8888"
 
 func main() {
 
 	// loading
 	configsrv()
 
-	utils.Abc("qqq")
-
 }
 
 func configsrv() {
 	flag.Parse()
-	fmt.Println("wyname", "ok")
-	fmt.Println("addr", *addr)
-	fmt.Println("port", *port)
-	fmt.Println("ssl", *ssl)
-
-	origin = origing()
-	fmt.Println("origin", origin)
-
-	kernel.Load(*addr, *port, *ssl)
-
-}
-
-func origing() string {
-	var o string
-	if *ssl {
-		o = "https://" + *addr
-	} else {
-		o = "http://" + *addr
-	}
-	return o
+	fmt.Println(*addr)
 }
