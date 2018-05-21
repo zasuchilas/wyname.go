695f573fbe41c6da481eb73bb0157d3a8ce6f19a
cho.chief <k.n.svetlakov@yandex.ru>
2018-05-21 19:58:05 +0400

    117 save

--- lifer.go
+++ lifer.go
@@ -57,7 +57,54 @@ func (l *Lifer) read() {
 		}
 		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
 		// c.hub.broadcast <- message
+		// TODO здесь нужно разбирать коды (inbox from browser)
 		log.Println("message: ", string(message))
+		l.send <- []byte("ko: " + string(message))
+	}
+}
+
+func (l *Lifer) write() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		l.conn.Close() // TODO дублирует то что в read ?
+	}()
+
+	for {
+		select {
+		case message, ok := <-l.send: // читаем из буфера записи лайфера
+			l.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// канал l.send был закрыт из сектора
+				l.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := l.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// добавляем остальные сообщения в очереди l.send (если есть)
+			// к данному сообщению (чтобы одним блоком ушло все из буфера лайфера)
+			// логично: ядер конечное число, и горутины ждут своей очереди
+			// поэтому используем возможность, чтобы не стоять снова
+			n := len(l.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-l.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C: // обработка ping/pong
+			l.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := l.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
 	}
 }
 
@@ -75,5 +122,5 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 	}
 
 	go lifer.read()
-
+	go lifer.write()
 }
--- sector.go
+++ sector.go
@@ -1,1 +1,8 @@
 package main
+
+// Sector регистрирует лайферов
+type Sector struct {
+	members     map[*Lifer]bool // члены сектора
+	subscribers map[*Lifer]bool // подписчики сектора
+
+}
