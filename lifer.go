0cac864df48a704620453b356ed3195344ea5852
cho.chief <k.n.svetlakov@yandex.ru>
2018-06-15 09:37:42 +0400

    save

--- lifer.go
+++ lifer.go
@@ -27,6 +27,7 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
 		log.Println("Origin:", r.Header.Get("Origin"))
+		log.Println("RemoteAddr:", r.RemoteAddr, "host:", r.Host, "Cookies:", r.Cookies(), "UserAgent:", r.UserAgent(), "Sec-WebSocket-Key:", r.Header.Get("Sec-WebSocket-Key"), "TLS:", r.TLS)
 		return true
 	},
 }
