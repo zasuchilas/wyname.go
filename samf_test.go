88341b0227af5823baa3903606c23c286086d15f
cho.chief <k.n.svetlakov@yandex.ru>
2018-06-22 11:27:46 +0400

    clear

--- samf_test.go
+++ samf_test.go
@@ -5,28 +5,6 @@ import (
 	"testing"
 )
 
-// sexfrom(samf)
-
-type sexfrompairs struct {
-	val int
-	res int
-}
-
-var sexfromtests = []sexfrompairs{
-	{sn | ac | fc, sn},
-	{sm, sm},
-	{sf | sn | fc, sn}, // если несколько значений в samf, вернуться должно sn
-}
-
-// func TestSexfrom(t *testing.T) {
-// 	for _, pair := range sexfromtests {
-// 		v := sexfrom(pair.val)
-// 		if v != pair.res {
-// 			t.Error("For", pair.val, "expected", pair.res, "got", v)
-// 		}
-// 	}
-// }
-
 // desamf
 
 type desamfpairs struct {
